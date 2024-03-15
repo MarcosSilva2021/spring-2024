352600ed2fe0c159f8d75f78eaf42ec1004d9ffe
marcosdsj <89165879+MarcosSilva2021@users.noreply.github.com>
2024-03-15 20:51:48 -0300

    add metodos : sub , mult e div

--- aulaSB1/src/main/java/br/edu/unichristus/aulaSB1/controller/CalculadoraController.java
+++ aulaSB1/src/main/java/br/edu/unichristus/aulaSB1/controller/CalculadoraController.java
@@ -3,6 +3,7 @@ package br.edu.unichristus.aulaSB1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.unichristus.aulaSB1.service.CalculadoraService;
@@ -14,10 +15,26 @@ public class CalculadoraController {
 	@Autowired
 	CalculadoraService service;
 	
-	@GetMapping()
-	public double somar(double n1, double n2) {
+	@GetMapping("/somar")
+	public double somar(@RequestParam("num1") double n1, double n2) {
 		return service.somar(n1, n2);
 	}
+	
+	@GetMapping("/sub")
+	public double sub(@RequestParam("num1") double n1, double n2) {
+		return service.sub(n1, n2);
+	}
+	
+	@GetMapping("/mult")
+	public double mult(@RequestParam("num1") double n1, double n2) {
+		return service.somar(n1, n2);
+	}
+	
+	@GetMapping("/div")
+	public double div(@RequestParam("num1") double n1, double n2) {
+		return service.somar(n1, n2);
+	}
+	
 	
 	
 }
